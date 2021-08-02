c137a601beaaef47dc1f8010f0ba226a7602fa2d
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-08-02 16:13:28 +0300

    Adding the marker

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const apiKey='pk.eyJ1IjoiY29kZXJwYXNzIiwiYSI6ImNrcWRwbnE1dDE4aDAybm92ZXZvMWZoNXYifQ.RwNOEzig0atTUkVBWvNBiQ';
-const mymap = L.map('map').setView([51.505, -0.09], 13);
+const mymap = L.map('map').setView([-0.505, 37.29], 8);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: apiKey,
 }).addTo(mymap);
+
+// Adding marker
+const marker= L.marker([-0.505,37.39]).addTo(mymap)
